6161d84c786feb114592da5018f36839a71d3450
Dantas Beyer <dantasbeyer@gotmail.com>
2023-04-13 11:27:55 +0200

    get the date and time

--- server/middleware/logger.js
+++ server/middleware/logger.js
@@ -5,3 +5,9 @@ const fs = require('fs')
 const fsPromises = require('fs').promises
 const path = require('path')
 
+
+//Any time i create a log message i get the date and time
+const logEvents = async (message, logFileName) => {
+    const dateTime = `${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}`
+    const logItem = `${dateTime}\t${uuid()}\t${message}\n`
+}
