b3d10aba62ce94464e8f6c39ede6395bfb88919c
Dantas Beyer <dantasbeyer@gotmail.com>
2023-04-13 12:24:59 +0200

    updated

--- server/middleware/logger.js
+++ server/middleware/logger.js
@@ -1,5 +1,5 @@
 const {format} = require('date-fns')
-const {v4: uuid} = require('uuid')
+const {v4: uuidv4} = require('uuid')
 
 const fs = require('fs')
 const fsPromises = require('fs').promises
@@ -9,5 +9,23 @@ const path = require('path')
 //Any time i create a log message i get the date and time
 const logEvents = async (message, logFileName) => {
     const dateTime = `${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}`
-    const logItem = `${dateTime}\t${uuid()}\t${message}\n`
+    const logItem = `${dateTime}\t${uuidv4()}\t${message}\n`
+
+    try {
+        if (!fs.existsSync(path.join(__dirname, '..', 'logs'))) {
+            await fsPromises.mkdir(path.join(__dirname, '..', 'logs'))
+        }
+        await fsPromises.appendFile(path.join(__dirname, '..', 'logs', 
+        logFileName), logItem)
+    }catch (err){
+        console.log(err)
+    }
 }
+
+const logger = (req, res, next) => {
+    logEvents (`${req.method}\t${req.url}\t${req.headers.origin}`,'reqlog.log')
+    console.log(`${req.headers} ${req.path}`)
+    next()
+}
+
+module.exports = {logEvents, logger}
